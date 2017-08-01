8fd985c00acb33b5ffd040baf794000abcbbf563
Max Tweddell <maxt.nz@gmail.com>
2017-08-02 11:54:04 +1200

    added client api

---
+++ client/api.js
@@ -0,0 +1,18 @@
+import request from 'superagent'
+
+const akurl = 'https://api.at.govt.nz/v2/notifications/'
+const apikey = {Ocp-Apim-Subscription-Key:'e211ee79ed0748cfb49e320c6a55d7b2'}
+export function getNotice (callback) {
+    request
+        .get(akurl)
+        .send(apikey)
+        .end((err, res) => {
+            if (err) {
+                callback(err)
+            } else {
+                callback(null, res.body)
+            }
+        })
+
+
+}
---
+++ server/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const
+router.get('/', (req, res) => {
+    res.send()
+})
+
+
+function getData () {
+
+}
