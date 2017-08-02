dccff1c7e409a8117ceabbfa2283fa3f3185d6a1
sethman6 <sethnitin6@gmil.com>
2017-08-02 12:08:28 +1200

    App rendering sertup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import {} from '../api'
+import data from 'data'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+
+export default class App extends React.Component {
+constructor (props){
+  super (props)
+  this.state = {
+  }
+  this.setData = this.setData.bind(this)
 }
 
-export default App
+componentDidMount() {
+  getData(this.setData)
+}
 
+getData (err , data){
+this.setstate(data : data)
+})
+
+render ()
+  return(
+    <div>
+    <h1>Auckland Transport Infomation Site</h1>
+    <h2>{this.set.data.title}</h2>
+    <h3> {this.set.data.description}</h3>
+    </div>
+
+  })
+
+
+export default App
