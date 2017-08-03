72232da37260132668db65e93f69e4f46c6042ac
sethman6 <sethnitin6@gmail.com>
2017-08-03 13:54:42 +1200

    dead code deleted and pic added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,47 +6,6 @@ const flexContainer = {
   display: "flex",
   flexDirection:"column"
 }
-
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       data: []
-//     }
-//     this.setData = this.setData.bind(this)
-//   }
-
-//   componentDidMount() {
-//     getNotice(this.setData)
-//   }
-
-//   setData(err, noticeData) {
-//     if (!err) {
-//       this.setState({ data: noticeData })
-//       console.log(noticeData)
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Sethman's Personal Project using React and API</h1>
-//         <div className="flex-container" style={flexContainer}>
-//           {this.state.data.map(notice => {
-//             return <Notice data = {notice} key = {notice.id}/>
-//           })}
-//         </div>
-
-//       </div>
-//     )
-//   }
-//  }
-// //         <div>
-// //           <img src="http://c8.alamy.com/comp/E0HER5/the-new-auckland-transport-electric-train-on-its-first-day-of-operation-E0HER5.jpg" />
-// //         </div>
-
-//New code
-
 const style = {
   width: "50%"
 }
@@ -79,9 +38,9 @@ export default class App extends React.Component {
     return (
       <div id='page'>
         <div className='title'><h1> Sethman's Personal Project using React and API</h1></div>
-        <div className='trump-button'>
+        <div className='Project-button'>
           <button onClick={this.handleOnClick}>
-            <img src="./Tronald_Dump.png" alt="Not working" style={style} />
+            <img src="./project1.png" alt="Not working" style={style} />
             {/*this would ebcome this.style*/}
           </button>
         </div>
@@ -90,7 +49,7 @@ export default class App extends React.Component {
             {this.state.quote}
           </p>
           <p>
-            - Tronald Dump
+            - Sethman
           </p>
         </div>}
 
