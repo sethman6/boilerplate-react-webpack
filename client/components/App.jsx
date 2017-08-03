710c845d60165cd6f1f805334fc534e1c920b465
sethman6 <sethnitin6@gmail.com>
2017-08-03 13:44:41 +1200

    new App.jsx code working-pic not working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,40 +7,96 @@ const flexContainer = {
   flexDirection:"column"
 }
 
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       data: []
+//     }
+//     this.setData = this.setData.bind(this)
+//   }
+
+//   componentDidMount() {
+//     getNotice(this.setData)
+//   }
+
+//   setData(err, noticeData) {
+//     if (!err) {
+//       this.setState({ data: noticeData })
+//       console.log(noticeData)
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Sethman's Personal Project using React and API</h1>
+//         <div className="flex-container" style={flexContainer}>
+//           {this.state.data.map(notice => {
+//             return <Notice data = {notice} key = {notice.id}/>
+//           })}
+//         </div>
+
+//       </div>
+//     )
+//   }
+//  }
+// //         <div>
+// //           <img src="http://c8.alamy.com/comp/E0HER5/the-new-auckland-transport-electric-train-on-its-first-day-of-operation-E0HER5.jpg" />
+// //         </div>
+
+//New code
+
+const style = {
+  width: "50%"
+}
+
 export default class App extends React.Component {
+
   constructor(props) {
     super(props)
+
     this.state = {
-      data: []
+      quote: ""
     }
-    this.setData = this.setData.bind(this)
+
+    this.setQuote = this.setQuote.bind(this)
+    this.handleOnClick = this.handleOnClick.bind(this)
+    //this.style = {styling}      this. refers to the class.
+  }
+  handleOnClick(e) {
+    getQuote(this.setQuote)
   }
 
-  componentDidMount() {
-    getNotice(this.setData)
-  }
-
-  setData(err, noticeData) {
+  setQuote(err, quoted) {
     if (!err) {
-      this.setState({ data: noticeData })
-      console.log(noticeData)
-    }
+      this.setState({ 'quote': quoted.value })
+    } else (console.log('quote not recieved'))
+
   }
 
   render() {
     return (
-      <div>
-        <h1>Sethman's Personal Project using React and API</h1>
-        <div className="flex-container" style={flexContainer}>
-          {this.state.data.map(notice => {
-            return <Notice data = {notice} key = {notice.id}/>
-          })}
+      <div id='page'>
+        <div className='title'><h1> Sethman's Personal Project using React and API</h1></div>
+        <div className='trump-button'>
+          <button onClick={this.handleOnClick}>
+            <img src="./Tronald_Dump.png" alt="Not working" style={style} />
+            {/*this would ebcome this.style*/}
+          </button>
         </div>
+        {this.state.quote && <div className='quote'>
+          <p key={1}>
+            {this.state.quote}
+          </p>
+          <p>
+            - Tronald Dump
+          </p>
+        </div>}
 
-      </div>
+      </div >
     )
   }
- }
-//         <div>
-//           <img src="http://c8.alamy.com/comp/E0HER5/the-new-auckland-transport-electric-train-on-its-first-day-of-operation-E0HER5.jpg" />
-//         </div>
+}
+
+
