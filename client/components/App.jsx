5a84d1f3f02578c7bc194db78762f99acc017150
sethman6 <sethnitin6@gmail.com>
2017-08-07 19:10:21 +1200

    picture added and button created, not working at the moment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Search } from './Search'
+import { searchBar } from './Search'
 import { getYoda } from '../api'
 
 export default class Yoda extends React.Component {
@@ -17,9 +17,31 @@ export default class Yoda extends React.Component {
     getYoda(this.setYoda)
   }
 
+  handleOnchange(e) {
+    searchBar(this.getSearch)
+  }
+
   setYoda(err, quoted) {
     if (!err) {
       this.setState({ 'quote': quotedvalue })
-   } else (console.log ('Not Happening'))
+    } else (console.log('Not Happening'))
   }
+
+  getSearch(err, quoted) {
+    if (!err) {
+      this.setState({ 'search': searchedvalue })
+    } else (console.log('Not Working'))
   }
+  render (){
+    return (
+  <div>
+    <div className = 'Title' ><h1> Sethman's Movie PRoject using React</h1> </div>
+    <div className = 'button'> </div>
+    <button onClick = {this.handleOnclick}> </button>
+    <img src = "./Movies.png" />
+    <button onChange = {this.state.search}>
+    </button>
+</div>
+    )
+  }
+}
